83f02d1e99f177535fa5ed59546b6100c839c633
Carlos <ocariocawebdesign@gmail.com>
2020-06-02 12:01:18 -0300

    Novo commit com alterações

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -16,9 +16,10 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -26,12 +27,12 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -44,3 +45,47 @@ var app = {
 };
 
 app.initialize();
+
+
+function carregarDom() {
+    //Guarda os valores
+    sessionStorage.setItem('cliente', document.form01.cliente.value);
+
+    sessionStorage.setItem('telefone', document.form01.telefone.value);
+
+    sessionStorage.setItem('email', document.form01.email.value);
+
+    sessionStorage.setItem('endereco', document.form01.endereco.value);
+    //console.log(sessionStorage.getItem('cliente'));
+    var form = document.querySelector("form");
+    var log = document.querySelector("#resRad");
+
+
+
+    document.addEventListener("DOMContentLoaded", function (event) {
+        console.log("DOM completamente carregado e analisado");
+    });
+
+    form.addEventListener("submit", function (event) {
+        var adicional = new FormData(form);
+        var output = "";
+        for (const entry of adicional) {
+            output = output + "\r" + entry[0] + ": " + entry[1] + " " + "\r";
+        };
+        //checado.innerHTML = "Teste";
+        resRad.innerText = output;
+        var resultado =
+            output; //document.getElementById('resRad'); //.innerText Pegou a saída das escolhas
+
+        event.preventDefault();
+        console.log(resultado);
+
+        resultado = resultado.replace(/[ ]/g, '%20');
+        document.getElementById("whatsapp-share-btt").href =
+            `https://api.whatsapp.com/send?phone=5521971025148&text=${resultado}`;
+
+
+    }, false);
+}
+
+carregarDom();
